87f08fee8dbfbdf1d3e3b4a36988763a83e7030a
juangabrielparedes <juan.gabriel.paredes@correounivalle.edu.co>
2024-06-17 12:21:52 -0500

    puntos

--- app/src/main/scala/proyecto/Aeropuerto.scala
+++ app/src/main/scala/proyecto/Aeropuerto.scala
@@ -10,4 +10,23 @@ def distancia(a: Aeropuerto, b: Aeropuerto): Double = {
   val d = R * Math.acos(Math.sin(x1) * Math.sin(x2) + Math.cos(x1) * Math.cos(x2) * Math.cos(y1 - y2))
   d
 }
+def tiempoVuelo(a: Aeropuerto, b: Aeropuerto, v: Double): Double = {
+  val d = distancia(a, b)
+  val t = d / v
+  t
+}
+
 case class Vuelo(Aln: String, Num: Int, Org: String, HS: Int, MS:Int, Dst: String, HL: Int, ML: Int, Esc:Int)
+def tiempoSalida(v: Vuelo): Double = {
+  val h = v.HS
+  val m = v.MS
+  val t = h + m / 60
+  t
+}
+def tiempoLlegada(v: Vuelo): Double = {
+  val h = v.HS
+  val m = v.MS
+  val t = h + m / 60
+  t
+}
+
